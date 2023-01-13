6da139871a3aa1250cb9c2fd3b8c1bcdde9da7c3
Roger <90182060+rogerUserGitHub@users.noreply.github.com>
2023-01-13 14:10:41 +0100

    add element to dashboard

--- wordle/src/Views/Dashboard.js
+++ wordle/src/Views/Dashboard.js
@@ -7,24 +7,22 @@ import Rating from '../Components/Rating.js';
 import AverageRating from '../Components/AverageRating.js';
 import Badge from '../Components/Badge.js';
 import ResponsiveAppBar from '../Components/AppBar.js';
-import ProgressBar from '../Components/ProgressBar.js';
 import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
 import ScoreboardIcon from '@mui/icons-material/Scoreboard';
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import StarIcon from '@mui/icons-material/Star';
 import ImageAvatars from '../Components/Avatar';
+import Footer from '../Components/Footer';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
-import { tableContainerClasses } from '@mui/material';
 
 const style = {
   bg: `p-7 bg-gradient-to-r from-[#13171f] to-[#3c2342]`,
-  container: `max-w-[650px] m-auto rounded-md shadow-2xl`,
-  container22: `flex flex-wrap bg-slate-200 p-4`,
-  heading: `text-3xl font-bold text-center p-1`,
-  containerKeyStats: `bg-slate-300 pb-4 shadow-xl`,
-  container33: `flex bg-slate-100 space-x-4 p-4 shadow-xl`,
+  container: `max-w-[650px] m-auto rounded-md shadow-2xl bg-slate-300`,
+  container22: `flex flex-wrap bg-slate-200`,
+  heading: `text-3xl font-bold text-center`,
+  container33: `flex bg-slate-100 space-x-4 shadow-xl`,
   item: `bg-blue-200 p-3 rounded-md`,
   avatar: `content-end`,
   //
@@ -50,6 +48,7 @@ const Dashboard = () => {
   const [bestScore, setBestScore] = useState(0);
   const [numberOfFinishedGames, setNumberOfFinishedGames] = useState(0);
   const [numberOfUnfinishedGames, setnumberOfUnfinishedGames] = useState(0);
+  const [numberTotalProfiles, setNumberTotalProfiles] = useState(0);
   const [numberTotalGames, setNumberTotalGames] = useState();
   const [percentageProgress, setPercentageProgress] = useState(70);
   const [winRatePerc, setWinRatePerc] = useState(0);
@@ -61,6 +60,8 @@ const Dashboard = () => {
     setScreenName(profileData?.screenName);
     setAvatar(profileData?.avatar);
   };
+
+  console.log(numberTotalGames);
 
   const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     height: 10,
@@ -84,8 +85,9 @@ const Dashboard = () => {
         snapshot?.docs?.forEach(doc => {
           profileData?.push({ ...doc?.data(), id: doc.id });
         });
+        const totalNumberProfiles = profileData.length;
+        setNumberTotalProfiles(totalNumberProfiles);
         result = profileData?.find(obj => obj?.uid === user?.uid);
-        setDocId(result?.id);
         setValues(result);
       })
       .catch(err => {
@@ -103,6 +105,8 @@ const Dashboard = () => {
         snapshot?.docs?.forEach(doc => {
           gameRecords?.push({ ...doc?.data(), id: doc.id });
         });
+        const totalNumberGames = gameRecords.length;
+        setNumberTotalGames(totalNumberGames);
         gameRecords?.forEach(record => {
           if (record?.uid === user?.uid) {
             gamerecordsOfLoggedInUser.push(record);
@@ -153,8 +157,7 @@ const Dashboard = () => {
     setBestScore(bestScore);
     setNumberOfFinishedGames(countFinishedGames);
     setnumberOfUnfinishedGames(countUnfinishedGames);
-    setWinRatePerc((countFinishedGames / gamerecords?.length) * 100);
-    setNumberTotalGames(gamerecords?.length);
+    setWinRatePerc(Math.floor((countFinishedGames / gamerecords?.length) * 100));
   };
 
   const determineBadgeAndProgress = () => {
@@ -211,47 +214,59 @@ const Dashboard = () => {
       <ResponsiveAppBar avatar={avatar} logout={logout} />
       <div className={style.bg}>
         <div className={style.container}>
-          <div className={style.containerKeyStats}>
-            <h3 className={style.heading}>Game Stats</h3>
+          <h3 className={style.heading}>Player Stats</h3>
+          <div className='container mx-auto '>
+            <div className='grid grid-cols-1 md:grid-cols-2 '>
+              <div className='bg-gray-800 pt-14 '>
+                <Badge badge={badge} />
+                <Box sx={{ flexGrow: 1 }}>
+                  <BorderLinearProgress
+                    variant='determinate'
+                    value={percentageProgress}
+                  />
+                </Box>
+              </div>
+              <div className='p-4 bg-gray-800'>
+                <p className='text-[30px] text-blue-600 font-mono'>
+                  {' '}
+                  <VideogameAssetIcon /> # of games
+                </p>
+                <p className='text-white font-mono font-bold'>{gamerecords?.length}</p>
+                <p className='text-[30px] text-blue-600 font-mono'>
+                  <ScoreboardIcon /> best score
+                </p>
+                <p className='text-white font-mono font-bold'>{bestScore} / 6 tries</p>
+                <p className='text-[30px] text-blue-600 font-mono'>
+                  <WorkspacePremiumIcon /> % win rate{' '}
+                </p>
+                <p className='text-white font-mono font-bold'>{winRatePerc}</p>
+                <p className='text-[30px] text-blue-600 font-mono'>
+                  <StarIcon /> rank{' '}
+                </p>
+                <p className='text-white font-mono font-bold'>xxxxx</p>
+              </div>
+            </div>
+
             <div className='container mx-auto'>
+              <h3 className={style.heading}>App Stats</h3>
               <div className='grid grid-cols-1 md:grid-cols-2'>
-                <div className='bg-gray-800 pt-14'>
-                  <Badge badge={badge} />
-                  <Box sx={{ flexGrow: 1 }}>
-                    <BorderLinearProgress
-                      variant='determinate'
-                      value={percentageProgress}
-                    />
-                  </Box>
+                <div className='p-4 bg-gray-700 text-green-700'>
+                  <p className='text-[30px] text-blue-700 font-mono'>
+                    # of active users
+                  </p>
+                  <p>{numberTotalProfiles}</p>
                 </div>
-                <div className='p-4 bg-gray-800'>
-                  <p className='text-[30px] text-blue-600 font-mono'>
-                    {' '}
-                    <VideogameAssetIcon /> # of games
+                <div className='p-4 bg-gray-700 text-green-700'>
+                  <p className='text-[30px] text-blue-700 font-mono'>
+                    # of games played
                   </p>
-                  <p className='text-white font-mono font-bold'>
-                    {gamerecords?.length}
-                  </p>
-                  <p className='text-[30px] text-blue-600 font-mono'>
-                    <ScoreboardIcon /> best score
-                  </p>
-                  <p className='text-white font-mono font-bold'>
-                    {bestScore} / 6 tries
-                  </p>
-                  <p className='text-[30px] text-blue-600 font-mono'>
-                    <WorkspacePremiumIcon /> % win rate{' '}
-                  </p>
-                  <p className='text-white font-mono font-bold'>{winRatePerc}</p>
-                  <p className='text-[30px] text-blue-600 font-mono'>
-                    <StarIcon /> rank{' '}
-                  </p>
-                  <p className='text-white font-mono font-bold'>xxxxx</p>
+                  <p>{numberTotalGames}</p>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className={style.containerKeyStats}>
+          {/* <div className={style.containerKeyStats}>
             <h3 className={style.heading}>Other</h3>
             <div className='container mx-auto'>
               <div className='grid grid-cols-1 md:grid-cols-2'>
@@ -270,22 +285,27 @@ const Dashboard = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className='container mx-auto'>
-            <div className='grid grid-cols-1 md:grid-cols-2'>
-              <div className='p-4 bg-gray-200 text-gray-700'>
-                <p>Left Column</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-              <div className='p-4 bg-gray-200 text-gray-700'>
-                <p>Right Column</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          {/* <div className={style.containerKeyStats}>
+            <div className='container mx-auto'>
+              <h3 className={style.heading}>Game Stats</h3>
+              <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div className='p-4 bg-gray-200 text-gray-700'>
+                  <p className='text-[30px] text-blue-600 font-mono'># of users</p>
+                  <p>{numberTotalProfiles}</p>
+                </div>
+                <div className='p-4 bg-gray-200 text-gray-700'>
+                  <p className='text-[30px] text-blue-600 font-mono'>
+                    # of games played
+                  </p>
+                  <p>{numberTotalGames}</p>
+                </div>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className='container mx-auto'>
+          {/* <div className='container mx-auto'>
             <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1'>
               <div className='p-4 bg-gray-200 text-gray-700'>
                 <p>Left Column</p>
@@ -309,9 +329,9 @@ const Dashboard = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className='container mx-auto'>
+          {/* <div className='container mx-auto'>
             <div className='grid grid-cols-1 md:grid-cols-4'>
               <div className='p-4 bg-gray-200 text-gray-700'>
                 <p>Left Column</p>
@@ -330,25 +350,23 @@ const Dashboard = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className={style.containerKeyStats}>
-            <h3 className={style.heading}>App Information</h3>
-            <div className='container mx-auto'>
-              <div className='grid grid-cols-1 md:grid-cols-2'>
-                <div className='bg-gray-200 pl-14 pt-14'>
-                  <ImageAvatars avatar={'pic8'} />
-                </div>
-                <div className='p-4 bg-green-100'>
-                  <p className='text-[30px] text-blue-700 font-mono'># of Games</p>
-                  <p className='text-red-500 font-mono'>{gamerecords?.length}</p>
-                  <p className='text-[30px] text-blue-700 font-mono'>Best Score</p>
-                  <p className='text-red-500 font-mono'>{bestScore}</p>
-                  <p className='text-[30px] text-blue-700 font-mono'>% Win Rate </p>
-                  <p className='text-red-500 font-mono'>{winRatePerc}</p>
-                  <p className='text-[30px] text-blue-700 font-mono'>Rank </p>
-                  <p className='text-red-500 font-mono'>xxxxx</p>
-                </div>
+          <h3 className={style.heading}>Word of the day</h3>
+          <div className='container mx-auto'>
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+              <div className='bg-gray-200 pl-14 pt-14'>
+                <ImageAvatars avatar={'pic8'} />
+              </div>
+              <div className='p-4 bg-green-100'>
+                <p className='text-[30px] text-blue-700 font-mono'># of Games</p>
+                <p className='text-red-500 font-mono'>{gamerecords?.length}</p>
+                <p className='text-[30px] text-blue-700 font-mono'>Best Score</p>
+                <p className='text-red-500 font-mono'>{bestScore}</p>
+                <p className='text-[30px] text-blue-700 font-mono'>% Win Rate </p>
+                <p className='text-red-500 font-mono'>{winRatePerc}</p>
+                <p className='text-[30px] text-blue-700 font-mono'>Rank </p>
+                <p className='text-red-500 font-mono'>xxxxx</p>
               </div>
             </div>
           </div>
@@ -369,7 +387,7 @@ const Dashboard = () => {
         </div>
       </div>
 
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
